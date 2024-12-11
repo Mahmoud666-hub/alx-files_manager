8e4373366193d4c98b034c6e158a840a1fd77d08
root <root@DESKTOP-SVJM9K1>
2024-12-11 12:56:08 +0200

    hhi

--- utils/db.js
+++ utils/db.js
@@ -9,10 +9,10 @@ const url = `mongodb://${HOST}:${PORT}`;
 class DBClient {
   constructor() {
     this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
-    this.client.connect().then(() => {
+    this.client.connect().then(function() {
       this.db = this.client.db(`${DATABASE}`);
-    }).catch((err) => {
-      console.log(err);
+    }).catch((erro) => {
+      console.log(erro);
     });
   }
 
