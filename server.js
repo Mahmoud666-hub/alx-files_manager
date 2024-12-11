05ca05fe972d177eaa6aaae420bc923079a2aabc
mahmoud <mahmoudmodi75@gmail.com>
2024-12-11 14:01:59 +0200

    man1ret

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use('/', router);
 
-app.listen(port, () => {
+app.listen(port, function() {
   console.log(`server running on port ${port}`);
 });
 
